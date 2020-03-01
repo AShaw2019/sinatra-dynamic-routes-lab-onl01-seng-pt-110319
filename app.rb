c66bc0da326b28c9d7ca041f5bb48e8bf4f2e31a
Ashley Twomey <aprier091012@gmail.com>
2020-03-01 01:16:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,20 +2,41 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do
-    "Hello, World!"
+  get "/reversename/:name" do
+    @user_name = params[:name]
+    "#{@user_name.reverse}"
   end
 
-  get '/name' do
-    "My name is Ashley"
+  get "/square/:number" do
+    @result = params[:number].to_i*params[:number].to_i
+    "#{@result}"
   end
 
-  get '/hometown' do
-    "My hometown is Ironton"
+  get "/say/:number/:phrase" do
+    @the_product = ""
+    @the_number = params[:number].to_i
+    @the_phrase = params[:phrase]
+    @the_number.times do
+      @the_product += @the_phrase
+      @the_product += "\n"
+    end
+    "#{@the_product}"
   end
 
-  get '/favorite-song' do
-    "My favorite song is Better Today"
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-end
+  get "/:operation/:number1/:number2" do
+    @number_1 = params[:number1].to_i
+    @number_2 = params[:number2].to_i
+    if params[:operation] == "add"
+      "#{@number_1+@number_2}"
+    elsif params[:operation] == "subtract"
+      "#{@number_1-@number_2}"
+    elsif params[:operation] == "multiply"
+      "#{@number_1*@number_2}"
+    elsif params[:operation] == "divide"
+      "#{@number_1/@number_2}"
+    end
+  end
