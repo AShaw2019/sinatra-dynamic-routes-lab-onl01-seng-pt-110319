2078baacdba9c9f89e3b243aae81822ddacff671
Ashley Twomey <aprier091012@gmail.com>
2020-03-01 01:12:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Ashley"
+  end
+
+  get '/hometown' do
+    "My hometown is Ironton"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is "
+  end
 
 end
